8941ab50fd4bfe1232a11cd2894f44f188e225b0
Shivam Goel <55030452+shivamgoel008@users.noreply.github.com>
2021-04-12 13:04:12 +0530

    firebase login register added

---
+++ app/src/main/java/com/example/expanse/Login/FirebaseLoginFragment.kt
@@ -0,0 +1,94 @@
+package com.example.expanse.Login
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.expanse.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_firebase_login.*
+
+
+class FirebaseLoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (this.auth.currentUser != null) {
+            FirebaseLoginFragmentDirections.actionFirebaseLoginFragmentToLoginFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_firebase_login, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notRegistered.setOnClickListener {
+            findNavController().navigate(FirebaseLoginFragmentDirections.actionFirebaseLoginFragmentToFirebaseRegisterFragment4())
+        }
+
+        login.setOnClickListener {
+            firebaseEmailLogin.error = null
+            firebasePasswordLogin.error = null
+
+            val email = firebaseEmailLogin.editText?.text.toString()
+            val pass = firebasePasswordLogin.editText?.text.toString()
+
+            if (validateInput(email, pass)) {
+                progressBar.visibility = View.VISIBLE
+
+                // authenticating user
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                    progressBar.visibility = View.INVISIBLE
+
+                    if (task.isSuccessful) {
+                        findNavController().navigate(FirebaseLoginFragmentDirections.actionFirebaseLoginFragmentToLoginFragment())
+                    } else {
+                        val toast = Toast.makeText(
+                            requireActivity(),
+                            "Authentication failed: ${task.exception?.message}",
+                            Toast.LENGTH_LONG
+                        )
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,0)
+                        toast.show()
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private fun validateInput(email: String, pass: String): Boolean {
+        var valid = true
+        if(email.isBlank()){
+            firebaseEmailLogin.error= "Please enter an email address"
+            valid = false
+        }
+        else if(pass.length<8){
+            firebasePasswordLogin.error = "Password show 8 character or more"
+            valid=false
+        }
+
+        return valid
+    }
+
+
+}
---
+++ app/src/main/java/com/example/expanse/Login/FirebaseRegisterFragment.kt
@@ -0,0 +1,87 @@
+package com.example.expanse.Login
+
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.expanse.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_firebase_login.*
+import kotlinx.android.synthetic.main.fragment_firebase_register.*
+
+
+class FirebaseRegisterFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_firebase_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        AlreadyRegistered.setOnClickListener {
+            findNavController().navigate(FirebaseRegisterFragmentDirections.actionFirebaseRegisterFragmentToFirebaseLoginFragment2())
+        }
+
+        register.setOnClickListener {
+            firebaseEmailRegister.error = null
+            firebasePasswordRegister.error = null
+
+            val email = firebaseEmailRegister.editText?.text.toString()
+            val pass = firebasePasswordRegister.editText?.text.toString()
+
+            if (validateInput(email, pass)) {
+                progressBar2.visibility = View.VISIBLE
+
+                // authenticating user
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                    progressBar2.visibility = View.INVISIBLE
+
+                    if (task.isSuccessful) {
+                        findNavController().navigate(FirebaseRegisterFragmentDirections.actionFirebaseRegisterFragmentToLoginFragment())
+                    } else {
+                        val toast = Toast.makeText(
+                            requireActivity(),
+                            "Authentication failed: ${task.exception?.message}",
+                            Toast.LENGTH_LONG
+                        )
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,0)
+                        toast.show()
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun validateInput(email: String, pass: String): Boolean {
+        var valid = true
+        if(email.isBlank()){
+            firebaseEmailRegister.error= "Please enter an email address"
+            valid = false
+        }
+        else if(pass.length<8){
+            firebasePasswordRegister.error = "Password show 8 character or more"
+            valid=false
+        }
+
+        return valid
+    }
+
+}
